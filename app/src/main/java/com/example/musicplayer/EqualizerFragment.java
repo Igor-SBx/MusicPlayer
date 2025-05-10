b0414c9759ecd02297607f8f384bde31f55f4a80
Joimar <joimarbgf@gmail.com>
2025-05-10 20:22:49 -0300

    adding 5 seekbars

--- app/src/main/java/com/example/musicplayer/EqualizerFragment.java
+++ app/src/main/java/com/example/musicplayer/EqualizerFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentResultListener;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -86,6 +87,7 @@ public class EqualizerFragment extends Fragment {
 
                     AudioEqualizer eq = AudioEqualizer.getInstance();
                     eq.setBandGain(0, gain); // Exemplo: altera a banda 0
+                    Log.d("Equalização", "SeekBar");
 //                    mediaPlayer.setVolume(volume, volume);
                 }
             }
